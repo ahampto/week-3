0fe309ca619d89b3ca2fe4c26782dfb21ca17e35
ahampto <ahampto@iu.edu>
2025-09-24 03:14:45 +0000

    3rd week

--- apputil.py
+++ apputil.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # update/add code below ...
 def fib(n):
+    """ Return the nth Fibonacci number. """
     if n == 0:
         return 0
     if n == 1:
@@ -12,6 +13,8 @@ def fib(n):
 
 
 def to_binary(n):
+    """ Return the binary representation of a non-negative integer n as a string. """
+     # Base case
     if n == 0:
         return "0"
     elif n == 1:
@@ -22,17 +25,22 @@ def to_binary(n):
         return to_binary(n // 2) + str(n % 2)
 
 def task1():
+    """ Load the dataset and return the number of missing values in each column, sorted in descending order. """
     return df.isnull().sum().sort_values(ascending=False)
     
 
 def task2():
+    """ Return a DataFrame with the total number of admissions per year. """
     return df.groupby('year').agg(total_admissions=('year', 'count'))
     print('data needed some cleaning, including renaming and dropping columns as well as converting data types')
 
 def task3():
+    """ Return a DataFrame with the average age of patients grouped by """
+
     return df.groupby('gender')['age'].agg(['mean'])
     print('Some cleaning had to be done, including removing rows with different genders and converting ages to numeric values.')
 
 
 def task4():
+    """ Return a Series with the top 5 most common professions among patients. """
     return df['profession'].value_counts()[:5]
